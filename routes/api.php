67afd392f4699ce5d0434269f3d4b66f88afd332
misestudiosgenerales <misestudiosgenerales@gmail.com>
2026-02-10 21:03:43 +0100

    Rate Limiting

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,15 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthTokenController;
 
 
-Route::post('/login', [AuthTokenController::class, 'login']);
+Route::middleware(['throttle:api'])->post('/login', [AuthTokenController::class, 'login']);
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', fn (Request $request) => $request->user());
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
+    Route::get('/me', fn(Request $request) => $request->user());
     Route::post('/logout', [AuthTokenController::class, 'logout']);
     Route::get('/my-recipes', [AuthTokenController::class, 'myRecipes']);
 });
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:api'])->group(function () {
     Orion::resource('recipes', RecipeController::class);
 });
